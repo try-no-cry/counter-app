ed2a1695b648826c77c37f92a1617c7f57aee74e
try-no-cry <39919696+try-no-cry@users.noreply.github.com>
2021-02-13 09:43:10 +0530

    first commit

---
+++ src/Components/counter.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class  Counter extends Component {
+    state = {  }
+    render() { 
+        return ( <h1>hey Boy</h1> );
+    }
+}
+ 
+
+export default Counter;
